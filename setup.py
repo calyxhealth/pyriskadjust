628f4f396d3946321fd8fc328977941ce20be50a
Kevin Matulef <matulef@gmail.com>
2019-01-11 18:45:05 -0800

    get packages from submodules

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     include_package_data=True,
     keywords='pyriskadjust',
     name='pyriskadjust',
-    packages=find_packages(include=['pyriskadjust']),
+    packages=find_packages(include=['pyriskadjust.*']),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
