b23e1ba4daa596a40c18b07e6746e6999a9d5c78
Kevin Matulef <matulef@gmail.com>
2019-01-16 22:12:03 -0800

    add script to reverse map hcc to icd code

---
+++ pyriskadjust/utils/hcc_to_mapping.py
@@ -0,0 +1,12 @@
+from pyriskadjust.icd_mapping.mapping_2018_v22 import ICD_MAPPING
+from pyriskadjust.hccs.hccs_v22 import HCC_LABELS
+
+import json
+
+output = dict((n,[]) for n in HCC_LABELS.keys())
+for icd, hccs in ICD_MAPPING.items():
+    for hcc in hccs:
+        output[hcc].append(icd)
+
+with open("hcc_to_icd_2018.json", "w") as f:
+    json.dump(output, f, indent=4)
