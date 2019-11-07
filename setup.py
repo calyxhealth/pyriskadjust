1539bfbc27717b397f6a05b16702fae90f3caa87
Kevin Matulef <matulef@gmail.com>
2019-11-07 12:15:18 -0800

    Bump version: 0.9.2 → 0.9.3

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/calyxhealth/pyriskadjust',
-    version='0.9.2',
+    version='0.9.3',
     zip_safe=False,
 )
